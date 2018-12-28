83a26ab8d667e07519a9720fb3bf9f4ba6cf5cdc
Suraj Sriwastava <suraj_sriwastava@dell.com>
2018-12-28 18:06:02 +0530

    DB connection

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -27,13 +29,14 @@ namespace PalTracker
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton(sp => new WelcomeMessage(
-            Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.")));
+            Configuration.GetValue<string>("WELCOME_MESSAGE", "Default.")));
             services.AddSingleton(st => new CloudFoundryInfo(
                 Configuration.GetValue<string>("PORT", "WELCOME_MESSAGE not configured."),
                 Configuration.GetValue<string>("MEMORY_LIMIT", "WELCOME_MESSAGE not configured."),
                 Configuration.GetValue<string>("CF_INSTANCE_INDEX", "WELCOME_MESSAGE not configured."),
                 Configuration.GetValue<string>("CF_INSTANCE_ADDR", "WELCOME_MESSAGE not configured.")));
-            services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+            services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/PalTracker/TimeEntryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace PalTracker
+{
+    public class TimeEntryContext : DbContext
+    {
+        public TimeEntryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TimeEntryRecord> TimeEntryRecords { get; set; }
+    }
+}
